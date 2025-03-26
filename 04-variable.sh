efb1f7064793cfbfd8f86555cda0af618657d35f
nirshang <ngodugunuri@gmail.com>
2025-03-26 16:12:14 -0600

    commit

---
+++ 04-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "$PERSON2: Hello $PERSON1, Very Good Morning"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, How are you $PERSON1"
+echo "$PERSON1: I am good, Thank you"
+echo "$PERSON2: Thank you, Bye"
+echo "$PERSON1: Bye"
