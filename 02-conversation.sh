fb4bbb37db50224368d276773273465ddb4e40fc
nirshang <ngodugunuri@gmail.com>
2025-03-26 14:34:06 -0600

    commit

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Ramesh: Hello Suresh, Good Morning"
+echo "Suresh: Hello Ramesh, Very Good Morning"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I am good, How are you Ramesh"
+echo "Ramesh: I am good, Thank you"
+echo "Suresh: Thank you, Bye"
+echo "Ramesh: Bye"
