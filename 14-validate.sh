97d74edad02b9c33b8f2d2a4f8a9905c229074f9
nirshang <ngodugunuri@gmail.com>
2025-03-27 16:28:50 -0600

    commit

--- 14-validate.sh
+++ 14-validate.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 USERID=$(id -u)
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ];then
+       echo "Please run this script with root priveleges"
+       exit 1
+    else
+       echo "You are root user"
+    fi
+}
+
 VALIDATE(){
     if [ $1 -ne 0 ];then
        echo "$2 is failed"
@@ -9,11 +18,8 @@ VALIDATE(){
     fi
 }
 
-if [ $USERID -ne 0 ];then
-  echo "Please run this script with root priveleges"
-  exit 1
-fi
-    
+CHECK_ROOT
+
 dnf list installed mysql 
 
 if [ $? -ne 0 ];then
