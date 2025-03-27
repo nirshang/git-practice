a534227f2f152837cddaabf754b0c77687b8555d
nirshang <ngodugunuri@gmail.com>
2025-03-27 13:57:40 -0600

    commit

--- 13-mysql-install.sh
+++ 13-mysql-install.sh
@@ -10,6 +10,12 @@ dnf list installed mysql
 if [ $? -ne 0 ];then
    echo "mysql is not installed, installing mysql"
    dnf install mysql -y
+   if [ $? -ne 0 ];then
+      echo "mysql installation failed"
+      exit 1
+    else
+      echo "mysql installation success"
+    fi
 else 
    echo "mysql is already installed, nothing to do"
 fi
