2dd82cc811b0c5bfa24b79f85f3b9b4f56d6c735
nirshang <ngodugunuri@gmail.com>
2025-03-26 19:22:36 -0600

    commit

--- 11.condition.sh
+++ 11.condition.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo -p "please enter the number"
+read -p "please enter the number"
 
 if [ $number -gt 20 ];
 then
 echo "number is greater than 20"
 else
 echo "number is less than 20"
-fi
