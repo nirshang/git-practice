90fc0e4287c9811734dc9d1f1b8d0639c17b7a37
nirshang <ngodugunuri@gmail.com>
2025-03-28 12:31:18 -0600

    commit

--- 18-redirectors.sh
+++ 18-redirectors.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-scripts"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename "$0" | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
 mkdir -p $LOGS_FOLDER
@@ -12,32 +13,33 @@ Y="\e[33m"
 N="\e[0m"
 
 CHECK_ROOT(){
-    if [ $USERID -ne 0 ];then
-       echo -e " $R Please run this script with root priveleges $N" &>> $LOG_FILE
+    if [ $USERID -ne 0 ]; then
+       echo -e "$R Please run this script with root privileges $N" &>> $LOG_FILE
        exit 1
     fi
 }
 
 VALIDATE(){
-    if [ $1 -ne 0 ];then
-       echo -e "$2 is .... $R failed $N" &>>$LOG_FILE
+    if [ $1 -ne 0 ]; then
+       echo -e "$2 is .... $R failed $N"
+       echo -e "$2 is .... $R failed $N" &>> $LOG_FILE
        exit 1
     else
-       echo -e "$2 is $G success $N" &>>$LOG_FILE
+       echo -e "$2 is $G success $N"
+       echo -e "$2 is $G success $N" &>> $LOG_FILE
     fi
 }
 
 CHECK_ROOT
 
-for package in $@
+for package in "$@"
 do
-   dnf list installed $package &>>$LOG_FILE
-   if [ $? -ne 0 ]
-   then
-       echo "The package $package is not installed...going to install it" &>>$LOG_FILE
-       sudo dnf install -y $package &>>$LOG_FILE
-       VALIDATE $? "installing package"
+   dnf list installed "$package" &>> $LOG_FILE
+   if [ $? -ne 0 ]; then
+       echo "The package $package is not installed...going to install it" | tee -a $LOG_FILE
+       dnf install -y "$package" &>> $LOG_FILE
+       VALIDATE $? "Installing package $package"
    else
-       echo -e "The package $package is already $y installed $N" &>>$LOG_FILE
-    fi
+       echo -e "The package $package is already $Y installed $N" | tee -a $LOG_FILE
+   fi
 done
