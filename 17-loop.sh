6090aef4eca4d426d16e4790bf66824b920e7f97
nirshang <ngodugunuri@gmail.com>
2025-03-27 21:31:56 -0600

    commit

--- 17-loop.sh
+++ 17-loop.sh
@@ -7,8 +7,7 @@ do
    then
        echo "The package $package is not installed...going to install it"
        sudo dnf install -y $package
-       if [ $? -ne 0 ]
-       then
+       if [ $? -ne 0 ];then
            echo "The package $package failed to install"
        else
            echo "The package $package installed successfully"
